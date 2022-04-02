9d40230f0b3d1508a5c52f6f741bb5019711e0f1
Harshit Vishnoi <5825564-HARDY8118@users.noreply.gitlab.com>
2022-04-02 18:10:57 +0530

    Initial commit

---
+++ src/Window.test.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Window from "./Window";
+import userEvent from "@testing-library/user-event";
+
+test("Checking window properties", () => {
+  let { container } = render(
+    <Window
+      id="test-window-1"
+      height={20}
+      width={20}
+      titleBar={{
+        icon: "☹",
+        title: "Test Window",
+        buttons: { minimize: true, maximize: true },
+      }}
+    />
+  );
+  // screen.debug();
+  const testWindow =
+    container.querySelector<HTMLDivElement>(".window-container");
+
+  expect(testWindow).not.toBeNull();
+  expect(testWindow?.getAttribute("id")).toBe("test-window-1");
+  expect(testWindow?.style.height).toBe("20px");
+  expect(testWindow?.style.width).toBe("20px");
+  expect(testWindow?.style.top).toBe("0px");
+  expect(testWindow?.style.left).toBe("0px");
+  expect(testWindow?.style.resize).toBe("none");
+
+  const testWindowTitleBar = testWindow?.querySelector(".title-bar");
+  expect(testWindowTitleBar).not.toBeNull();
+
+  const testWindowTitleBarIcon = testWindowTitleBar?.querySelector(".icon");
+  expect(testWindowTitleBarIcon).not.toBeNull();
+  expect(testWindowTitleBarIcon?.textContent).toBe("☹");
+
+  const testWindowTitleBarTitle =
+    testWindowTitleBar?.querySelector(".windowTitle");
+  expect(testWindowTitleBarIcon).not.toBeNull();
+
+  const titleBarButtons =
+    testWindowTitleBar?.querySelector<HTMLDivElement>(".buttonContainer");
+  expect(titleBarButtons).not.toBeNull();
+
+  const minimizeButton = titleBarButtons?.querySelector<HTMLSpanElement>(
+    ".windowButton:nth-child(1)"
+  );
+  expect(minimizeButton).not.toBeNull();
+  expect(minimizeButton?.textContent).toBe("▁");
+
+  const maximizeButton = titleBarButtons?.querySelector<HTMLSpanElement>(
+    ".windowButton:nth-child(2)"
+  );
+  expect(maximizeButton).not.toBeNull();
+  expect(maximizeButton?.textContent).toBe("□");
+});
+
+test("Autogeneratting window id", () => {
+  let { container } = render(<Window />);
+
+  const testWindowAutoId =
+    container.querySelector<HTMLDivElement>(".window-container");
+
+  expect(testWindowAutoId).not.toBeNull();
+  expect(testWindowAutoId?.getAttribute("id")).toMatch(/[0-9]{13}/);
+});
---
+++ src/Window.tsx
@@ -0,0 +1,241 @@
+import React, { useRef } from "react";
+import "./Window.css";
+
+interface WindowProps {
+  id: string;
+  children?: any;
+  height: number;
+  width: number;
+  top?: number;
+  left?: number;
+  resizable?: boolean;
+  titleBar?: {
+    icon?: string | HTMLImageElement;
+    title?: string;
+    buttons?: {
+      minimize?: boolean;
+      maximize?: boolean;
+      close?: () => void;
+    };
+  };
+}
+
+const nextZIndex: () => number = () => {
+  let maxZ = 0;
+  for (let w of document.querySelectorAll<HTMLDivElement>(
+    ".window-container"
+  )) {
+    let z = parseInt(w.style.zIndex);
+
+    maxZ = Math.max(isNaN(z) ? 0 : z, maxZ);
+  }
+  return maxZ + 1;
+};
+
+const Window = (props: WindowProps) => {
+  let properties = Object.assign(
+    {
+      id: props.id && props.id.length ? props.id : Date.now().toString(),
+      children: null,
+      height: 0,
+      width: 0,
+      top: 0,
+      left: 0,
+      resizable: false,
+      titleBar: Object.assign(
+        {
+          icon: " ",
+          title: "Untitled window",
+          buttons: Object.assign(
+            {
+              minimize: true,
+              maximize: true,
+              close: () => {},
+            },
+            (props.titleBar && props.titleBar.buttons) || {}
+          ),
+        },
+        props.titleBar
+      ),
+    },
+    props
+  );
+
+  if (!properties.id) {
+    properties.id = Date.now().toString();
+  }
+
+  Object.freeze(properties);
+
+  const [height, setHeight] = React.useState(properties.height);
+  const [width, setWidth] = React.useState(properties.width);
+  const [top, setTop] = React.useState<number>(properties.top || 0);
+  const [left, setLeft] = React.useState<number>(properties.left || 0);
+  const [xOffset, setXOffset] = React.useState<number>(0);
+  const [yOffset, setYOffset] = React.useState<number>(0);
+  const [minimized, setMinimized] = React.useState<boolean>(false);
+  const [maximized, setMaximized] = React.useState<boolean>(false);
+  const [minimizeIcon, setMinimizeIcon] = React.useState<string>("▁");
+  const [maximizeIcon, setMaximizeIcon] = React.useState<string>("□");
+  const [contentDisplay, setContentDisplay] = React.useState<boolean>(true);
+  const [windowTransition, setWindowTransition] = React.useState("");
+  const [level, setLevel] = React.useState<number>(nextZIndex());
+
+  const container = React.useRef(null);
+  const titleBar = React.useRef(null);
+  const effectiveHeight = useRef(height);
+  const effectiveWidth = useRef(width);
+
+  const animationDuration = 500;
+
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    setYOffset(e.clientY - top);
+    setXOffset(e.clientX - left);
+    setLevel(nextZIndex());
+  };
+
+  const handleMove = (e: React.DragEvent<HTMLDivElement>) => {
+    setLeft(e.clientX - xOffset);
+    setTop(e.clientY - yOffset);
+  };
+
+  const minimize = () => {
+    setWindowTransition(`${animationDuration}ms ease-in-out`);
+    const parent = document.getElementById(properties.id)?.parentElement;
+    if (minimized) {
+      setContentDisplay(true);
+      effectiveHeight.current = height;
+      setTop(parent?.offsetTop || 0);
+      setLeft(parent?.offsetLeft || 0);
+      setMinimized(false);
+      setMinimizeIcon("▁");
+      setMaximized(false);
+    } else {
+      setContentDisplay(false);
+      effectiveHeight.current = 32;
+      const parent = document.getElementById(properties.id)?.parentElement;
+      effectiveWidth.current = width;
+      let topPosition =
+        (parent?.clientHeight || window.innerHeight) -
+        effectiveHeight.current -
+        4;
+
+      let leftPosition =
+        (parent?.clientWidth || window.innerWidth) - effectiveWidth.current - 4;
+
+      const minimizedWindow = document.elementFromPoint(
+        leftPosition + effectiveWidth.current / 2,
+        topPosition + effectiveHeight.current / 2
+      ) as HTMLDivElement;
+      if (
+        minimizedWindow &&
+        ["window-container", "windowTitle"].includes(
+          minimizedWindow?.className || ""
+        )
+      ) {
+        topPosition -= minimizedWindow?.clientHeight + 4;
+      }
+
+      setTop(topPosition);
+      setLeft(leftPosition);
+      setMinimized(true);
+      setMinimizeIcon("◰");
+      setMaximized(false);
+    }
+    setLevel(nextZIndex());
+    setTimeout(setWindowTransition, animationDuration + 1, "");
+  };
+
+  const maximize = () => {
+    setWindowTransition(`${animationDuration}ms ease-in-out`);
+    const parent = document.getElementById(properties.id)?.parentElement;
+    if (maximized) {
+      setContentDisplay(true);
+      effectiveHeight.current = height;
+      effectiveWidth.current = width;
+      setTop(parent?.offsetTop || 0);
+      setLeft(parent?.offsetLeft || 0);
+      setMaximized(false);
+      setMaximizeIcon("□");
+      setMinimized(false);
+      setMinimizeIcon("▁");
+    } else {
+      setContentDisplay(true);
+      effectiveHeight.current = parent?.clientHeight || window.innerHeight;
+      effectiveWidth.current = parent?.clientWidth || window.innerWidth;
+      setTop(parent?.offsetTop || 0);
+      setLeft(parent?.offsetLeft || 0);
+      setMaximized(true);
+      setMaximizeIcon("❐");
+      setMinimized(false);
+      setMinimizeIcon("▁");
+    }
+    setLevel(nextZIndex());
+    setTimeout(setWindowTransition, animationDuration + 1, "");
+  };
+
+  return (
+    <div
+      id={properties.id}
+      className="window-container"
+      style={{
+        height: effectiveHeight.current,
+        width: effectiveWidth.current,
+        top,
+        left,
+        resize: properties.resizable ? "both" : "none",
+        transition: windowTransition,
+        zIndex: level,
+      }}
+      ref={container}
+      onClick={() => {
+        setLevel(nextZIndex());
+      }}
+    >
+      {properties.titleBar && (
+        <div className="title-bar" ref={titleBar}>
+          {properties.titleBar.icon && (
+            <span className="icon">{properties.titleBar.icon}</span>
+          )}
+          {properties.titleBar.title && (
+            <span
+              className="windowTitle"
+              draggable={true}
+              onDragStart={handleDragStart}
+              onDrag={handleMove}
+              onDragEnd={handleMove}
+            >
+              {properties.titleBar.title}
+            </span>
+          )}
+          {properties.titleBar.buttons && (
+            <span className="buttonContainer">
+              {properties.titleBar.buttons.minimize && (
+                <span className="windowButton" onClick={minimize}>
+                  {minimizeIcon}
+                </span>
+              )}
+              {properties.titleBar.buttons.maximize && (
+                <span className="windowButton" onClick={maximize}>
+                  {maximizeIcon}
+                </span>
+              )}
+              {<span className="windowButton">&#10799;</span>}
+            </span>
+          )}
+        </div>
+      )}
+      <div
+        className="content"
+        draggable="false"
+        style={{
+          height: contentDisplay ? "auto" : 0,
+        }}
+      >
+        {properties.children}
+      </div>
+    </div>
+  );
+};
+
+export default Window;
